99c530adcced4a3399ebda3e9016b290b75d9073
rigole <foplacide@gmail.com>
2024-05-20 14:48:27 +0100

    seeting the project with tailwindcss and react and typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './Header/Header';
+import { Routes, Route } from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Header/Header.tsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+
+
+
+const  Header = () => {
+
+/*const [isActive, setIsActive] = useState<boolean>(false);
+const [menuDisplay, setMenuDisplay] = useState<boolean>(false);
+const registered = useSelector((state:any) => state.userRegister);
+const { isOpen, toggle } = useModal();
+const loggedUser = useSelector((state:any) => state.userSignIn);
+*/
+
+//const { userInfo }  =  loggedUser
+
+//const { registeredUser  } = registered
+
+//const dispatch = useDispatch()
+
+//console.log(registeredUser);
+
+/*const logoutHandler = () => {
+    dispatch<any>(logout())
+}*/
+
+/*const changeIcon = () => {
+    setIsActive(current => !current);
+}*/
+
+/*const toggleOpen = () => {
+    changeIcon();
+    setMenuDisplay(!menuDisplay);
+    const menu = document.querySelector("div.main_menu") as HTMLDivElement;
+    const list = document.querySelector("ul.testing_list") as HTMLElement;
+
+    menu.style.transition = "0.5s";
+    menu.style.width="250px";
+
+    list.style.transition="0.5s";
+
+    if (isActive){
+        menu.style.width="0";
+    }
+
+
+}
+
+function ModalComponent(){
+    const { isOpen, toggle } = useModal();
+}
+*/
+
+    return (
+        <div className="fixed w-full z-10">
+            This is the header
+            <nav className=" w-full px-5 absolute list-none  right-0 left-0 z-10 text-blue-400 bg-[#1a1a37]">
+                <div className="mx-auto flex py-5 justify-between font-comf">
+                    <div>
+                        <Link to="/" className="text-lg  py-2 px-4 rounded-md bg-transparent text-white transition-all"> Reviewers</Link>
+                    </div>
+                </div>
+            </nav>
+            
+        </div>
+    )
+} 
+
+
+export default Header
