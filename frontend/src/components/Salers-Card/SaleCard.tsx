d38902c0320157fe861f27fc1be934b35da553c6
rigole <foplacide@gmail.com>
2024-06-29 17:23:42 +0100

    setting the card of the saler details

--- frontend/src/components/Salers-Card/SaleCard.tsx
+++ frontend/src/components/Salers-Card/SaleCard.tsx
@@ -3,16 +3,21 @@ import Rating from "../Rating/Rating";
 
 const SaleCard = () => {
     return (
-        <div className="relative saler_single_card w-[40%] h-[40%]  shadow-[0_30px_30px_0_rgba(0,0,0,0.5)] rounded-2xl bg-slate-950">
-            <div className="saler-infos flex px-2 py-3">
-                <div className="pp_university bg-white h-[75px] w-[75px] shadow-[0_5px_5px_0_rgba(0,0,0,0.5)]  rounded-2xl">
-                    <img src={salerImage} className="rounded-full h-full w-full" alt="" />
-                </div>
+        <div className="relative saler_single_card w-[65%] h-[40%] px-3  shadow-[0_30px_30px_0_rgba(0,0,0,0.5)] rounded-2xl bg-slate-950">
+            <div className="saler-infos flex justify-between px-2 py-3">
+                <div className="flex">
+                    <div className="pp_university bg-white h-[75px] w-[75px] shadow-[0_5px_5px_0_rgba(0,0,0,0.5)]  rounded-2xl">
+                        <img src={salerImage} className="rounded-full h-full w-full" alt="" />
+                    </div>
 
-                <div className="text-white px-6 py-1" >
-                    <p >Name of the saler</p>
-                    <Rating color='#e0e0e0' value={3.5} text="Score Reviews 3.5"  />
-                    <p>Nombre de reviews: 55</p>
+                    <div className="text-white px-6 py-1" >
+                        <p >Name of the saler</p>
+                        <Rating color='#e0e0e0' value={3.5} text="Score Reviews 3.5"  />
+                        <p>Nombre de reviews: 55</p>
+                    </div>
+                </div>
+                <div className="text-white location  flex justify-between">
+                    <span className="px-5"> <i className="fa-sharp fa-solid fa-location-pin px-2"></i>Douala</span>
                 </div>
             </div>
             
@@ -25,6 +30,14 @@ const SaleCard = () => {
             </div>
 
 
+            <div className="text-white py-3 flex justify-between">
+                <span ><i className="fa fa-address-card" aria-hidden="true"></i> informations</span>
+
+                <p>Categorie: categorie</p>
+
+                <p> Avis recent <i className="fa fa-arrow-down" aria-hidden="true"></i></p>
+
+            </div>
             
             
         </div>
